4cffffe28b1d4ff02d0df59b0d85026d0a250852
joana <joanamartasouza@gmail.com>
2025-09-18 16:26:06 -0300

    18/09/2025

--- src/main/java/com/example/Cadastro_Aluno/config/SwaggerConfig.java
+++ src/main/java/com/example/Cadastro_Aluno/config/SwaggerConfig.java
@@ -1,21 +1,23 @@
 package com.example.Cadastro_Aluno.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.;
 
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public OpenApi oficinaOpenAPI(){
-        return new OpenAPI()
-                .info(new Info())
-                .title("API-Cursos Tecnicos e Livres");
-                .description("Cadastro de Alunos na plataforma tanto tecnico e cursos livres");
-                .version("1.0");
-        .contact(new Contact()
-                .name("Secretaria dos Cursos")
-                .email("senai@sesi.com"));
 
+    @Bean
+    public OpenAPI oficinaOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API - Cursos Técnicos e Livres")
+                        .description("Cadastro de Alunos na plataforma tanto técnico quanto cursos livres")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Secretaria dos Cursos")
+                                .email("senai@sesi.com")));
     }
 }
--- src/main/java/com/example/Cadastro_Aluno/controller/AlunoController.java
+++ src/main/java/com/example/Cadastro_Aluno/controller/AlunoController.java
@@ -1,6 +1,7 @@
 package com.example.Cadastro_Aluno.controller;
 
 import com.example.Cadastro_Aluno.Aluno;
+import entity.CadastroAluno;
 import org.springframework.web.bind.annotation.*;
 import domain.entity.service.CadastroAlunoService;
 
@@ -8,26 +9,27 @@ import java.util.List;
 
 public class AlunoController {
     private final CadastroAlunoService alunoService;
-    public AlunoController(CadastroAlunoService cadastroAlunoService, CadastroAlunoService alunoService){
+    public AlunoController
+            (CadastroAlunoService cadastroAlunoService, CadastroAlunoService alunoService){
         this.alunoService = alunoService;
         CadastroAlunoService alunoService1 = this.alunoService;
 
     }
     @GetMapping
-    public List<Aluno> listar(){
+    public List<CadastroAluno> listar(){
         return alunoService.listarTodos();
     }
     @GetMapping("/{id}")
-    public Aluno buscar(@PathVariable Long id){
+    public CadastroAluno buscar(@PathVariable Long id){
         return  alunoService.buscarPorId(id);
     }
     @PostMapping
-    public Aluno cadastrar(@RequestBody Aluno aluno){
+    public CadastroAluno cadastrar(@RequestBody Aluno aluno){
         return alunoService.salvar(aluno);
     }
     @PutMapping
-    public Aluno atualizar(@PathVariable Long id, @RequestBody Aluno novoAluno){
-        Aluno alunoExistente = alunoService.buscarPorId(id);
+    public CadastroAlunoatualizar(@PathVariable Long id, @RequestBody Aluno novoAluno){
+        CadastroAluno alunoExistente = alunoService.buscarPorId(id);
         if(alunoExistente != null){
             alunoExistente.setNome(novoAluno.getNome());
             alunoExistente.setIdade(novoAluno.getIdade());
